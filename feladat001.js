98f2aad2c94c281e3043a4c9e159d43af177b833
suzo777 <73218354+suzo777@users.noreply.github.com>
2026-07-23 16:28:36 +0200

    Update feladat001.js

--- feladat001.js
+++ feladat001.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
     console.log(req.url);
-    res.send("Hello World!\n");
+    res.send("Hello Express World!\n");
 });
 app.listen(4000, () => {
     console.log("A szerver elindult a 4000-es porton.");
